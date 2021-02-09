c505698a74a82e9d8be9f79f6ac4d6518f7ed4ad
Christopher Brown <112749+chriskbrown@users.noreply.github.com>
2021-02-09 18:16:59 -0500

    another ride on the static files merry go round

--- sfswitch/settings.py
+++ sfswitch/settings.py
@@ -138,8 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-    os.path.join(BASE_DIR, 'sfswitch/static/'),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../sfswitch/static'),
 ]
 
 
