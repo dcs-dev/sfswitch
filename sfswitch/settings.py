58671e7a76f2b520dea6137dd582af738033ef93
Christopher Brown <112749+chriskbrown@users.noreply.github.com>
2021-02-09 18:25:52 -0500

    Make sure the static root directory exists

--- sfswitch/settings.py
+++ sfswitch/settings.py
@@ -136,6 +136,9 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+os.makedirs(config['STATIC_ROOT'], exist_ok=True)
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
