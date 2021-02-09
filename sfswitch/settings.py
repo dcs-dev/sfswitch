db7d686209a67dca0bbbffdf2f2fc0b8b9ed1bc7
Christopher Brown <112749+chriskbrown@users.noreply.github.com>
2021-02-09 18:30:24 -0500

    fix error with static tmp dir creation

--- sfswitch/settings.py
+++ sfswitch/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 os.makedirs(STATIC_TMP, exist_ok=True)
 
-os.makedirs(config['STATIC_ROOT'], exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
