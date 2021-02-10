dcfd5adbc1954502b4b358af1a89efd8ced7780e
Christopher Brown <112749+chriskbrown@users.noreply.github.com>
2021-02-09 20:35:08 -0500

    settings update for static files

--- sfswitch/settings.py
+++ sfswitch/settings.py
@@ -137,7 +137,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_TMP = os.path.join(BASE_DIR, 'staticfiles')
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
